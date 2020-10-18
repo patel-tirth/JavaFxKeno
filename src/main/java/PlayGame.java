41f730f04d982f0804bf99b42e78aa65d8f271ab
tpatel79 <tpatel79@uic.edu>
2020-10-18 16:09:16 -0400

    Updated

--- src/main/java/PlayGame.java
+++ src/main/java/PlayGame.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.PauseTransition;
+import javafx.animation.Timeline;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -26,15 +31,21 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.util.Duration;
 
 public class PlayGame {
 //	
 //	BorderPane borderPane = new BorderPane();
-//	
+	public int num =0 ;
+	public int totalNum = 20;
+	public int j;
 	private MenuBar gameMenu2;
+	int time= 0 ;
 	public int count = 0;  // to keep track on total number of spots selected from bet grid (1,4,8,10)
 	String spotNumber; // string to store the spot selected
-	ArrayList<Integer> NumberList= new ArrayList<Integer>(); 
+//	ArrayList<Integer> NumberList= new ArrayList<Integer>(); 
+	HashMap<Integer, Boolean> NumberMap = new HashMap<Integer, Boolean>();
+	HashMap<Integer, Boolean> gridValue = new HashMap<Integer, Boolean>();
 
 	 public PlayGame() {
 		// TODO Auto-generated constructor stub
@@ -147,7 +158,8 @@ public class PlayGame {
 				 grid.setDisable(true);
 			 }
 			 int numberOfSpots =Integer.parseInt(spotNumber);  
-			 fillOutSpots(grid,numberOfSpots);
+			 fillOutSpots(grid,numberOfSpots, gamePane);
+			 
 		});
 		
 		
@@ -156,27 +168,110 @@ public class PlayGame {
 	
 	 }
 	 
+	 // Function to generate a set of 20 random numbers
+	 public void generateRandNum(GridPane grid, HashMap<Integer, Boolean> NumberMap,Pane gamePane)
+	 {
+		 System.out.println(NumberMap.keySet());
+		 
+		 
+		j =1 ;   // grid starts from 1
+		 Random rand = new Random();
+		 HashMap<Integer,Boolean> randomNumbers = new HashMap<Integer,Boolean>();
+		 for(int i =0 ; i <totalNum; ++i)
+		 {
+			 num = rand.nextInt(80);
+			 randomNumbers.put(num,true);
+			 if (randomNumbers.size() <= 20) {
+	                if (randomNumbers.size() == 20) {
+	                    totalNum = 10;
+	                }
+	                totalNum++;
+	                randomNumbers.put(num,true);
+	            }
+		 }
+		 
+		 grid.getChildren().forEach(node -> { 
+			 
+			 PauseTransition pause = new PauseTransition(Duration.millis(200+(time*300)));
+			 if(gridValue.containsKey(j)== true && randomNumbers.containsKey(j))
+			 {
+				 pause.play();
+				 pause.setOnFinished(e->node.setStyle("-fx-background-color: red;"));
+			 } 
+			 if(NumberMap.containsKey(j)== true && randomNumbers.containsKey(j) )
+			 {
+				 pause.play();
+				 pause.setOnFinished(e->node.setStyle("-fx-background-color: green;"));
+			 }
+			 time++;
+			 j++;
+			 
+		 });
+ 
+	 }
+	 
 	 // function to fill the bet card
-	 public void fillOutSpots(GridPane grid, int spotNumber)
+	 public void fillOutSpots(GridPane grid, int spotNumber,Pane gamePane)
 	 { 
 		
 		  System.out.println(spotNumber); 
+		  
 		  	 
 			  grid.getChildren().forEach(node -> {   // for each node in the grid pane,
 				                                     // let user click any random number based on spots selected
 				  node.setOnMouseClicked(e->{
-					  if(count >=spotNumber)
+					  if(count == spotNumber && ((ToggleButton)e.getSource()).isSelected() == false)
+					  {
+						  node.setStyle(null);    // allow user to select any numbers
+						  String text = ((ToggleButton)e.getSource()).getText();
+						  int betNum =Integer.parseInt(text);
+						  NumberMap.remove(betNum);
+
+					  }
+					  else if(count >=spotNumber) {
+						
 						  return;
-					  node.setStyle("-fx-background-color: #d442f5;");
-					  node.setDisable(true);
-					  String text = ((Button)e.getSource()).getText();
+					  }
+					  
+					  if(((ToggleButton)e.getSource()).isSelected() == false)
+					  {
+						  String text = ((ToggleButton)e.getSource()).getText();
+						  int betNum =Integer.parseInt(text);  
+						  node.setStyle(null);
+						 NumberMap.remove(betNum);
+
+						  count--;
+					  }
+					  else {
+					 node.setStyle("-fx-background-color: #3489eb;");
+					  String text = ((ToggleButton)e.getSource()).getText();
 					  int betNum =Integer.parseInt(text);  
-					  NumberList.add(betNum);    // add the selected bet number to array NumberList
+					  NumberMap.put(betNum,true);    // add the selected bet number to array NumberList
+
 					  count++;
+					  }
+
 				  });
 				  
 			  });
 			  
+//			  HBox drawBox = new HBox(draw);
+//			  
+//			  
+			  Button draw = new Button("Draw");
+				 gamePane.getChildren().add(draw);
+//			  drawBox.setAlignment(Pos.CENTER_RIGHT);
+			  
+//			  BorderPane root = new BorderPane();
+//			    root.setPadding(new Insets(20)); // space between elements and window border
+//			    root.setBottom(drawBox);
+//			    gamePane.getChildren().addAll(draw);
+			  draw.setOnAction(e->{
+////				  grid.setDisable(true);
+//				  draw.setStyle("-fx-cursor: hand;");
+				  generateRandNum(grid,NumberMap,gamePane);
+				  
+			  });
 	 }
 	 
 	 // function to make the grid 
@@ -191,7 +286,9 @@ public class PlayGame {
 			for(int row = 0; row<=7; row++) {
 				for(int column = 1; column<=10; column++) {
 					count++;
-					Button b1 = new Button(Integer.toString(count));
+					gridValue.put(count,true);
+					ToggleButton b1 = new ToggleButton(Integer.toString(count));
+					
 					grid.add(b1, row, column);
 					 
 				}	
